dc99181c162a6ae8a0b60d40255495891fadda3e
Anata0820 <anata_almaty@yahoo.com>
2023-05-16 11:13:10 +1200

    provision ecr

---
+++ ecr.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+    region = "ap-southeast-2"
+}
+resource "aws_ecr_repository" "ecr_repo" {
+    name = "image-terraform" 
+    image_scanning_configuration {
+        scan_on_push = true 
+    }
+}
+
+/*
+resource "aws_iam_user_policy_attachment" "attach-user" {
+    user = github-actions
+    policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+} */
